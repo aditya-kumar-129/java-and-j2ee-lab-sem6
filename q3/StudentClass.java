7ede538303723e5d65f9c157060e98e3c16eef00
aditya-kumar-129 <adityakumar93340@gmail.com>
2022-06-25 21:12:43 +0530

    Added code for q3 by me

--- q3/StudentClass.java
+++ q3/StudentClass.java
@@ -1,122 +1,126 @@
 package q3;
 
-import java.awt.*;
-import java.awt.event.*;
-import java.util.LinkedList;
-import javax.swing.*;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import java.util.ArrayList;
 
 public class StudentClass implements ActionListener {
-  LinkedList<Student> studentList = new LinkedList<Student>();
+  JLabel NAME = new JLabel("Enter Name of the student :- ");
+  JLabel USN = new JLabel("Enter usn of the student :- ");
+  JLabel AGE = new JLabel("Enter age of the student :- ");
+  JLabel ADDRESS = new JLabel("Enter address of the student :- ");
+  JLabel CATEGORY = new JLabel("Select Category of the student :- ");
+  JLabel SGPA1 = new JLabel("Enter SGPA1 of the student :- ");
+  JLabel SGPA2 = new JLabel("Enter SGPA2 of the student :- ");
+  JLabel SGPA3 = new JLabel("Enter SGPA3 of the student :- ");
+  JLabel SGPA4 = new JLabel("Enter SGPA4 of the student :- ");
+  JLabel SGPA5 = new JLabel("Enter SGPA5 of the student :- ");
+  JLabel SGPA6 = new JLabel("Enter SGPA6 of the student :- ");
+  JLabel SGPA7 = new JLabel("Enter SGPA7 of the student :- ");
+  JLabel SGPA8 = new JLabel("Enter SGPA8 of the student :- ");
+  JLabel CGPA = new JLabel("CGPA of the student ");
 
-  // Defining all the levels that are required
-  JLabel Name = new JLabel("Enter Name of the Student :- ");
-  JLabel USN = new JLabel("Enter USN of the Student :- ");
-  JLabel Age = new JLabel("Enter Age of the Student :- ");
-  JLabel Address = new JLabel("Enter Address at which Student Lives :- ");
-  JLabel Category = new JLabel("Select category of the Student :- ");
-  JLabel SGPA1 = new JLabel("Enter SGPA of I sem");
-  JLabel SGPA2 = new JLabel("Enter SGPA of II sem");
-  JLabel SGPA3 = new JLabel("Enter SGPA of III sem");
-  JLabel SGPA4 = new JLabel("Enter SGPA of IV sem");
-  JLabel SGPA5 = new JLabel("Enter SGPA of V sem");
-  JLabel SGPA6 = new JLabel("Enter SGPA of VI sem");
-  JLabel SGPA7 = new JLabel("Enter SGPA of VII sem");
-  JLabel SGPA8 = new JLabel("Enter SGPA of VIII sem");
-  JLabel CGPA = new JLabel("CGPA obtained");
-
-  // Adding the textFeild
-  JTextField name = new JTextField(20);
-  JTextField usn = new JTextField(20);
-  JTextField age = new JTextField(3);
-  JTextArea address = new JTextArea(3, 4);
+  JTextField name = new JTextField(10);
+  JTextField usn = new JTextField(10);
+  JTextField age = new JTextField(10);
+  JTextField address = new JTextField(10);
+  // A littile different
   JComboBox<String> category = new JComboBox<String>();
-  JTextField cgpa = new JTextField(10);
-  JTextField sgpa1 = new JTextField(5);
-  JTextField sgpa2 = new JTextField(5);
-  JTextField sgpa3 = new JTextField(5);
-  JTextField sgpa4 = new JTextField(5);
-  JTextField sgpa5 = new JTextField(5);
-  JTextField sgpa6 = new JTextField(5);
-  JTextField sgpa7 = new JTextField(5);
-  JTextField sgpa8 = new JTextField(5);
 
-  JButton submit = new JButton("COMPUTE");
-  JButton done = new JButton("done");
+  JTextField sgpa1 = new JTextField(10);
+  JTextField sgpa2 = new JTextField(10);
+  JTextField sgpa3 = new JTextField(10);
+  JTextField sgpa4 = new JTextField(10);
+  JTextField sgpa5 = new JTextField(10);
+  JTextField sgpa6 = new JTextField(10);
+  JTextField sgpa7 = new JTextField(10);
+  JTextField sgpa8 = new JTextField(10);
+  JTextField cgpa = new JTextField(10);
+
+  JButton submit = new JButton("Submit");
   JButton display = new JButton("display");
-  // JTextArea(int row, int column) Creates a text area with the specified number
-  // of rows and columns that displays no text initially.
+  JButton done = new JButton("done");
+
+  // This will be used for frame f2 for dispalying the details of the student
   JTextArea stud_list_display = new JTextArea(20, 20);
-  // JTextArea(int row, int column) Creates a text area with the specified number
-  // of rows and columns that displays no text initially.
+
   JFrame f1 = new JFrame("Student Information");
   JFrame f2 = new JFrame("Student Collection Display");
 
-  StudentClass() {
-    Name.setBounds(10, 10, 10, 10);
-    category.addItem("GM");
-    category.addItem("SC/ST");
-    category.addItem("Cat1");
-    category.addItem("Cat2");
-    f1.add(Name);       f1.add(name);
-    f1.add(USN);        f1.add(usn);
-    f1.add(Age);        f1.add(age);
-    f1.add(Address);    f1.add(address);
-    f1.add(Category);   f1.add(category);
-    f1.add(SGPA1);      f1.add(sgpa1);
-    f1.add(SGPA2);      f1.add(sgpa2);
-    f1.add(SGPA3);      f1.add(sgpa3);
-    f1.add(SGPA4);      f1.add(sgpa4);
-    f1.add(SGPA5);      f1.add(sgpa5);
-    f1.add(SGPA6);      f1.add(sgpa6);
-    f1.add(SGPA7);      f1.add(sgpa7);
-    f1.add(SGPA8);      f1.add(sgpa8);
-    f1.add(CGPA);       f1.add(cgpa);
-    f1.add(submit);     f1.add(done);      f1.add(display);
+  ArrayList<Student> studentList = new ArrayList<>();
+
+  public StudentClass() {
+    category.addItem("SC");
+    category.addItem("ST");
+    category.addItem("OBC");
+    category.addItem("GENERAL");
+    f1.add(NAME);        f1.add(name);
+    f1.add(USN);         f1.add(usn);
+    f1.add(AGE);         f1.add(age);
+    f1.add(ADDRESS);     f1.add(address);
+    f1.add(CATEGORY);    f1.add(category);
+    f1.add(SGPA1);       f1.add(sgpa1);
+    f1.add(SGPA2);       f1.add(sgpa2);
+    f1.add(SGPA3);       f1.add(sgpa3);
+    f1.add(SGPA4);       f1.add(sgpa4);
+    f1.add(SGPA5);       f1.add(sgpa5);
+    f1.add(SGPA6);       f1.add(sgpa6);
+    f1.add(SGPA7);       f1.add(sgpa7);
+    f1.add(SGPA8);       f1.add(sgpa8);
+    f1.add(CGPA);        f1.add(cgpa);
+    f1.add(submit);      f1.add(display);      f1.add(done);
+
     f2.add(stud_list_display);
-    // Sets the size of the frame
-    f1.setSize(900, 800);
-    // GridLayout(int rows, int columns): creates a grid layout with the given rows
-    // and columns but no gaps between the components.
+
+    f1.setSize(500, 500);
     f1.setLayout(new GridLayout(8, 8));
-    // Sets the frame to be visible
     f1.setVisible(true);
+
     submit.addActionListener(this);
-    done.addActionListener(this);
     display.addActionListener(this);
+    done.addActionListener(this);
   }
 
   public void actionPerformed(ActionEvent event) {
-    // https://www.oreilly.com/library/view/java-for-dummies/9781118239742/a2_08_9781118239742-ch05.html
-    if (event.getSource() == submit) {
+    if (event.getSource() == "submit") {
       try {
-        int v1 = Integer.parseInt(age.getText());
-        if (v1 < 18 || v1 > 35) {
-          // It basically displays a dialoguebox in which we ask the user to re enter the age which should is valid.
-          // The re-entered age should be set to the form.
+        int entered_user_age = Integer.parseInt(age.getText());
+        if (entered_user_age < 18 || entered_user_age > 35) {
           String age1 = JOptionPane.showInputDialog(null, "Enter valid Age");
           age.setText(age1);
         }
-      } catch (NumberFormatException e) {
+      } catch (Exception e) {
         JOptionPane.showMessageDialog(f1, "Invalid entry");
         age.requestFocus();
       }
-      checkSGPA_valid(1, sgpa1);
-      checkSGPA_valid(2, sgpa2);
-      checkSGPA_valid(3, sgpa3);
-      checkSGPA_valid(4, sgpa4);
-      checkSGPA_valid(5, sgpa5);
-      checkSGPA_valid(6, sgpa6);
-      checkSGPA_valid(7, sgpa7);
-      checkSGPA_valid(8, sgpa8);
-      float calculate_CGPA = calculate_cgpa();
-      cgpa.setText(Float.toString(calculate_CGPA));
-    } else if (event.getSource() == done)// to submit into collection
-    {
+      validate_SGPA(1, sgpa1);
+      validate_SGPA(2, sgpa2);
+      validate_SGPA(3, sgpa3);
+      validate_SGPA(4, sgpa4);
+      validate_SGPA(5, sgpa5);
+      validate_SGPA(6, sgpa6);
+      validate_SGPA(7, sgpa7);
+      validate_SGPA(8, sgpa8);
+      float calculated_cgpa = calculateCGPA();
+      cgpa.setText(Float.toString(calculated_cgpa));
+    } 
+    else if (event.getSource() == "display") {
       Student newStudentObject = new Student(name.getText(), usn.getText(), address.getText(),
-          String.valueOf(category.getSelectedItem()), 
+          String.valueOf(category.getSelectedItem()),
           Integer.parseInt(age.getText()), Float.parseFloat(cgpa.getText()));
       studentList.add(newStudentObject);
-    } else {
+    } 
+    else {
       f1.setVisible(false);
       f2.setVisible(true);
       f2.setSize(500, 500);
@@ -126,32 +130,33 @@ public class StudentClass implements ActionListener {
     }
   }
 
-  float calculate_cgpa() {
-    float v1 = Float.parseFloat(sgpa1.getText());
-    float v2 = Float.parseFloat(sgpa2.getText());
-    float v3 = Float.parseFloat(sgpa3.getText());
-    float v4 = Float.parseFloat(sgpa4.getText());
-    float v5 = Float.parseFloat(sgpa5.getText());
-    float v6 = Float.parseFloat(sgpa6.getText());
-    float v7 = Float.parseFloat(sgpa7.getText());
-    float v8 = Float.parseFloat(sgpa8.getText());
-    return (v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8) / 8;
+  float calculateCGPA() {
+    float value_sgpa1 = Float.parseFloat(sgpa1.getText());
+    float value_sgpa2 = Float.parseFloat(sgpa2.getText());
+    float value_sgpa3 = Float.parseFloat(sgpa3.getText());
+    float value_sgpa4 = Float.parseFloat(sgpa4.getText());
+    float value_sgpa5 = Float.parseFloat(sgpa5.getText());
+    float value_sgpa6 = Float.parseFloat(sgpa6.getText());
+    float value_sgpa7 = Float.parseFloat(sgpa7.getText());
+    float value_sgpa8 = Float.parseFloat(sgpa8.getText());
+    return (value_sgpa1 + value_sgpa2 + value_sgpa3 + value_sgpa4 + value_sgpa5 + value_sgpa6 + value_sgpa7 + value_sgpa8) / 8;
   }
 
-  void checkSGPA_valid(int sem, JTextField sgpa) {
+  void validate_SGPA(int sem, JTextField sgpa) {
+    float value_sgpa = Float.parseFloat(sgpa.getText());
     try {
-      if (Float.parseFloat(sgpa.getText()) > 10) {
-        String v1 = JOptionPane.showInputDialog(null, "Enter an SGPA less than or equal to 10 for sem " + sem);
-        sgpa.setText(v1);
+      if (value_sgpa > 10) {
+        String correct_sgpa = JOptionPane.showInputDialog(null, "Enter an SGPA less than or equal to 10 for sem " + sem);
+        sgpa.setText(correct_sgpa);
       }
-    } catch (NumberFormatException e) {
+    } catch (Exception e) {
       String v2 = JOptionPane.showInputDialog(null, "Please enter SGPA for semester " + sem);
       sgpa.setText(v2);
-      // sgpa.requestFocus();
+      sgpa.requestFocus();
     }
   }
 
-  public static void main(String args[]) {
+  public static void main(String[] args) {
     StudentClass sc = new StudentClass();
   }
 }
